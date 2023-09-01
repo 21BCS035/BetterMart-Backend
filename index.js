3d3bd8d774ae359535032bb6737fa13491267980
Arpit <ar5136874yadav@gmail.com>
2023-09-01 23:41:19 +0530

    new changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dbConnect = require("./config/dbConnect")
+const mongoose=require("mongoose")
 const app = express();
 const dotenv = require("dotenv").config();
 const authRouter = require("./routes/authRoute")
@@ -18,9 +19,22 @@ const cookieParser = require("cookie-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cors = require("cors")
 const PORT = process.env.PORT || 4000;
-dbConnect();
+// dbConnect();
 
-app.use(morgan('dev'));
+const DB = process.env.MONGODB_CLUSTER.replace(
+    "<password>",
+    process.env.CLUSTER_PASSWORD
+  );
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    // useCreateIndex:true,
+    // useFindAndModify:false
+  })
+  .then(() => console.log(`Connection sucessfully`));
+
+// app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
